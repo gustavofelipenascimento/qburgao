f6b255c5e5f18b82df9abaf0249a2530c7094322
Gustavo Felipe do Nascimento <gufenascimento@gmail.com>
2023-05-24 14:56:03 -0300

    Att nova

---
+++ lanchonete/backend/app/controllers/produto.controller.js
@@ -0,0 +1,10 @@
+const produtoModel = require("../models/produto.model.js");
+
+exports.create = (req, res) => {}
+exports.findAll = (req, res) => {}
+exports.findById = (req, res) => {}
+exports.update = (req, res) => {}
+exports.delete = (req, res) => {}
+exports.deleteAll = (req, res) => {}
+
+
