6f1f8a4df828d0eab56a73e55b73b7e16e809d96
Gustavo Felipe do Nascimento <gufenascimento@gmail.com>
2023-05-31 14:55:57 -0300

    Ajuste no create | Model do Update criado

--- backend/app/models/produto.model.js
+++ backend/app/models/produto.model.js
@@ -5,9 +5,37 @@ const ProdutoModel = function (produto) {
     this.valor = produto.valor;
 }
 //Cria novo produto no banco
-ProdutoModel.create = (produto, result) => { };
+ProdutoModel.create = (produto, result) => {
+    sql.query("insert into produtos set ?", produto, (err, res) => {
+        if (err){
+            console.log("Erro: ", err);
+            result(err, null);
+            return;
+        }
+
+        console.log("Produto criado: ", {idprodutos: res.insertId, ...produto});
+        result(null, {idprodutos: res.insertId, ...produto});
+    })
+};
+
+
 //Seleciona produto por ID
 ProdutoModel.findById = (produtoId, result) => {
+    
+    sql.query("SELECT * FROM produtos where idprodutos = "+produtoId, (err,res) => {
+        if (err) {
+            console.log("Erro: ", err);
+            result(null, err);
+            return;
+    }
+    if (res.length){
+        console.log("Produto Encontrado", res[0]);
+        result(null, res[0]);
+    }else {
+        result({type: "not_found"},null);
+        console.log("Produto não encontrado");
+    }
+ })
 };
 //Seleciona todos os produtos
 ProdutoModel.getAll = result => {
@@ -23,7 +51,20 @@ ProdutoModel.getAll = result => {
 
 };
 //Atualizar produto por id
-ProdutoModel.updateById = (produtoId, produto, result) => { };
+ProdutoModel.updateById = (produtoId, produto, result) => {
+    sql.query("UPDATE produtos SET nome = ?, valor = ? WHERE idprodutos"),
+        [produto.nome, produto.valor, produtoId], (err, res) => {
+            if(err){
+                console.log("erro: ",err);
+                result(null, err);
+            }else if (res.affectdRows == 0){
+                result({ type: "not_found"}, null);
+            }else {
+                console.log("Produto Atualizado", {idprodutos: produtoId, ...produto});
+                result(null, {idprodutos})
+            }}
+
+ };
 //Remover produto por id
 ProdutoModel.remove = (produtoId, result) => { };
 //Remover todos os produtos
